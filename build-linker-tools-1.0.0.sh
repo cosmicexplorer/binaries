aa836b47eed1d09f0c8a16f3a33c898498032aa2
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-02-21 22:31:38 -0800

    add linker script (lol)

---
+++ build-linker-tools-1.0.0.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+# only for osx: this is a path to an osx binary for llvm-config, which is needed
+# to compile cctools. After running ./build-llvm-5.0.1.sh, llvm-config should be
+# located at something like:
+# llvm-build/clang+llvm-5.0.1-final-x86_64-apple-darwin/bin/llvm-config
+LLVM_CONFIG_LOCATION="$1"
+
+# Put safe mode below extracting arguments so we don't choke if this is run with
+# no arguments.
+set -euxo pipefail
+
+LINKER_TOOLS_SUPPORTDIR='build-support/bin/linker-tools'
+LINKER_TOOLS_PANTS_ARCHIVE_NAME='linker-tools.tar.gz'
+LINKER_TOOLS_VERSION='1.0.0'
+LINKER_TOOLS_BUILD_TMP_DIR='linker-tools'
+
+# default to -j2
+MAKE_JOBS="${MAKE_JOBS:-2}"
+
+
+## Linux (binutils, from source release)
+BINUTILS_VERSION='2.30'
+BINUTILS_TMP_ARCHIVE_CREATION_DIR='binutils-tmp'
+
+function make_binutils {
+  if ! hash xz; then
+    cat >&2 <<EOF
+'xz' is required to run this script. You may have to install it using your
+operating system's package manager.
+EOF
+    exit 1
+  fi
+
+  mkdir -p "$LINKER_TOOLS_BUILD_TMP_DIR"
+  pushd "$LINKER_TOOLS_BUILD_TMP_DIR"
+
+  curl -L -O "https://ftpmirror.gnu.org/binutils/binutils-${BINUTILS_VERSION}.tar.xz"
+  tar xf "binutils-${BINUTILS_VERSION}.tar.xz"
+  pushd "binutils-${BINUTILS_VERSION}"
+
+  ./configure
+  make -j"$MAKE_JOBS"
+
+  popd
+
+  rm -rf "$BINUTILS_TMP_ARCHIVE_CREATION_DIR"
+  mkdir "$BINUTILS_TMP_ARCHIVE_CREATION_DIR"
+  pushd "$BINUTILS_TMP_ARCHIVE_CREATION_DIR"
+
+  mkdir bin
+  cp "../binutils-${BINUTILS_VERSION}/ld/ld-new" bin/ld
+  tar cvzf "$LINKER_TOOLS_PANTS_ARCHIVE_NAME" \
+      bin/ld
+  local linker_tools_linux_packaged="$(pwd)/${LINKER_TOOLS_PANTS_ARCHIVE_NAME}"
+
+  popd
+
+  popd
+
+  mkdir -p "${LINKER_TOOLS_SUPPORTDIR}/linux/x86_64/${LINKER_TOOLS_VERSION}"
+  cp "$linker_tools_linux_packaged" "${LINKER_TOOLS_SUPPORTDIR}/linux/x86_64/${LINKER_TOOLS_VERSION}/${LINKER_TOOLS_PANTS_ARCHIVE_NAME}"
+}
+
+
+
+## MacOS (cctools, from github source)
+MACOS_REVS=(
+  10.7
+  10.8
+  10.9
+  10.10
+  10.11
+  10.12
+  10.13
+)
+
+CCTOOLS_REPO_URL='https://github.com/tpoechtrager/cctools-port'
+CCTOOLS_SHA='e527b6f87f0613de7ec6d214f81d41fb5621a5b0'
+CCTOOLS_TMP_ARCHIVE_CREATION_DIR='cctools-tmp'
+
+function make_cctools {
+  local prevpath="$PATH"
+  PATH="/bin:/usr/bin:${prevpath}"
+
+  mkdir -p "$LINKER_TOOLS_BUILD_TMP_DIR"
+  pushd "$LINKER_TOOLS_BUILD_TMP_DIR"
+
+  rm -rf 'cctools-port'
+  git clone --depth 1 "$CCTOOLS_REPO_URL"
+  pushd 'cctools-port'
+  git checkout "$CCTOOLS_SHA"
+
+  pushd 'cctools'
+
+  ./configure --with-llvm-config="$LLVM_CONFIG_LOCATION"
+
+  make -j"$MAKE_JOBS"
+
+  popd
+  popd
+
+  mkdir -p "$CCTOOLS_TMP_ARCHIVE_CREATION_DIR"
+  pushd "$CCTOOLS_TMP_ARCHIVE_CREATION_DIR"
+
+  mkdir -p bin
+  cp '../cctools-port/cctools/ld64/src/ld/ld' bin/ld
+  tar cvzf "$LINKER_TOOLS_PANTS_ARCHIVE_NAME" \
+      bin/ld
+  local linker_tools_macos_packaged="$(pwd)/${LINKER_TOOLS_PANTS_ARCHIVE_NAME}"
+  popd
+
+  popd
+
+  for rev in ${MACOS_REVS[@]}; do
+    dest_base="${LINKER_TOOLS_SUPPORTDIR}/mac/${rev}/${LINKER_TOOLS_VERSION}"
+    mkdir -p "$dest_base"
+    cp "$linker_tools_macos_packaged" "${dest_base}/${LINKER_TOOLS_PANTS_ARCHIVE_NAME}"
+  done
+
+  PATH="${prevpath}"
+}
+
+
+## Run the appropriate build function.
+cur_plat="$(uname)"
+
+case "$cur_plat" in
+  ( Darwin )
+  if [[ ! -x "$LLVM_CONFIG_LOCATION" ]]; then
+    cat >&2 <<EOF
+The first argument '$LLVM_CONFIG_LOCATION' does not exist or is not an
+executable file. Please run this script again with the first argument pointing
+to a binary for the llvm-config tool.
+EOF
+    exit 1
+  fi
+  make_cctools
+  ;;
+  ( Linux )
+  make_binutils
+  ;;
+  ( * )
+  echo "unrecognized platform '${cur_plat}'" >&2
+  exit 1
+esac
